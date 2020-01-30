7803163b957df6b98a616e7e7487d90233fe9666
DHih <57665806+DHih@users.noreply.github.com>
2020-01-29 23:20:58 -0300

    multiplicação
    Utilizando a multiplicação como saida de dados

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
+import java.util.Locale;
 import java.util.Scanner;
 
+//nao esqueca de mudar o nome da classe para Main
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double R, A, pi = 3.14159;
+	    
+	    R = sc.nextDouble();
 
-		Scanner sc = new Scanner (System.in);
-		
-	int a = sc.nextInt();
-	int b = sc.nextInt();
-int y= a+b;
-System.out.println("X = "+y);
-		
-		
-sc.close();
+	    A = pi * R * R;
+
+	    System.out.printf("A=%.4f%n", A);
+
+		sc.close();
 	}
-
 }
