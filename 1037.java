3c99b7a7fbd9a2746508135c4d51f09141aaa92d
DHih <57665806+DHih@users.noreply.github.com>
2020-03-09 23:09:59 -0300

    Rename Main.java to 1037.java

--- 1037.java
+++ 1037.java
@@ -1,9 +1,9 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-//Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo 
+//VocÃª deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo 
 //em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente 
-//se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+//se o valor nÃ£o estiver em nenhum destes intervalos, deverÃ¡ ser impressa a mensagem â€œFora de intervaloâ€.
 public class Main {
 
 	public static void main(String[] args) {
